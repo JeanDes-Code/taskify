9ea6ca32195663c280b13d10c07d1fc8c45effec
Jean Desauw <desauwjean@gmail.com>
2023-11-27 12:05:18 +0100

    Board server action

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -16,20 +16,42 @@ import { CreateBoard } from "./schema";
  * @return {Promise<ReturnType>} The created board or an error object.
  */
 const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
-  if (!userId) {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
     return {
       error: "Unauthorized",
     };
   }
 
-  const { title } = data;
+  const { title, image } = data;
+
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+    image.split("|");
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageLinkHTML ||
+    !imageUserName
+  ) {
+    return {
+      error: "Missing fields. Failed to create board.",
+    };
+  }
+
   let board;
 
   try {
     board = await db.board.create({
       data: {
         title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
       },
     });
   } catch (error) {
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { ElementRef, useRef } from "react";
+import { X } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
 import {
   Popover,
   PopoverContent,
@@ -12,8 +17,7 @@ import { createBoard } from "@/actions/create-board";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
-import { X } from "lucide-react";
-import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -28,22 +32,27 @@ export const FormPopover = ({
   align,
   side = "bottom",
 }: FormPopoverProps) => {
+  const router = useRouter();
+
+  const closeRef = useRef<ElementRef<"button">>(null);
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log(data);
       toast.success("Board created");
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.log(error);
       toast.error(error);
     },
   });
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    const image = formData.get("image") as string;
 
     execute({
       title,
+      image,
     });
   };
 
@@ -59,7 +68,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose asChild ref={closeRef}>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral 600"
             variant="ghost"
@@ -70,6 +79,7 @@ export const FormPopover = ({
 
         <form className="space-y-4" action={onSubmit}>
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board Title"
